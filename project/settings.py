bc8e0e5bc87a2aa29b35ab0a385be770bea0ff7f
tafursj17 <tatangolo2448@gmail.com>
2023-02-20 19:37:18 -0500

    Ingreso de Variables de Entorno

--- project/settings.py
+++ project/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -80,9 +81,9 @@ WSGI_APPLICATION = 'project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'project_cf',
-        'USER': 'root',
-        'PASSWORD': 'Server24.h',
+        'NAME': config("DATABASE_NAME"),
+        'USER': config("DATABASE_USER"),
+        'PASSWORD': config("DATABASE_PASSWORD"),
         'HOST': 'localhost',
         'PORT': '3306'
     }
